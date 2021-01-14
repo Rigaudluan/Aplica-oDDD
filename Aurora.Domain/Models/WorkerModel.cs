96db3756fddd6060a1eeadbaf0c3af88e73ab5db
Luan <rigaudluanf@gmail.com>
2021-01-14 12:44:39 -0300

    first commit

---
+++ Aurora.Domain/Models/WorkerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aurora.Domain.Models
+{
+    public class WorkerModel
+    {
+        public WorkerModel(int id, string name, DateTime birthDate, string cpf, string Namorada)
+        {
+            Id = id;
+            Name = name;
+            BirthDate = birthDate;
+            Nin = cpf;
+            Girlfriend = Namorada;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Nin { get; set; }
+        public string Girlfriend { get; set; }
+    }
+}
